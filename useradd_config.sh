e0d30d93db253f2d08e55b6786416c891bad25a3
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-09-10 21:02:58 -0500

    Add files via upload

---
+++ useradd_config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "You must be root to run this script" >&2
+        exit 1
+fi
+
+
+path="/etc/default/useradd"
+
+values=('HOME' 'INACTIVE' 'EXPIRE' 'SKEL' 'CREATE_MAIL_SPOOL')
+
+declare -A configs=(
+        [HOME]="/home"
+        [INACTIVE]=14
+        [EXPIRE]=-1
+        [SKEL]="/etc/skel"
+        [CREATE_MAIL_SPOOL]="yes"
+)
+
+for i in "${values[@]}"; do
+
+        if grep -q "^${i}" "$path"; then
+                sed -i "s/^\(${i}[[:space:]]*\).*/\1 ${configs[$i]}/" "$path"
+        else
+                echo "${i} ${configs[$i]}" >> "$path"
+        fi
+done
