fafa7835457ab02d40999ec7d2e53e5165eeaea8
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-10-14 21:41:30 -0500

    Add files via upload

---
+++ create_user.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "You must be root to run this script"
+        exit 1
+fi
+
+
+Add-User() {
+
+        local username="$1"
+
+        local password
+        password="${RANDOM}${RANDOM}"
+
+        if [[ -z "$username" ]]; then
+                echo "Username not provided"
+                echo "Usage: $0 <username>"
+                exit 1
+        fi
+
+        if id "$username" &>/dev/null; then
+                echo "User '$username' already exits"
+                exit 1
+        fi
+
+        if ! useradd -m -c "User: $username" "$username"; then
+                echo "Failed to creat user."
+                exit 1
+        fi
+
+        if ! echo "$username:$password" | chpasswd; then
+                echo "Failed to set password for $username"
+                exit 1
+        fi
+
+        echo "User '$username' has been created."
+        echo "User '$username' password is: $password"
+}
+
+
+
+Add-User "$1"
